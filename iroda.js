9eb5cf24ae6dd790a30cab4ebec34bacc8232c95
faxriddinovichazeez <faxriddinovichazeez@gmail.com>
2023-01-26 18:04:21 +0500

    fix

---
+++ iroda.js
@@ -0,0 +1,79 @@
+//1
+//  let num = [1,2,3]
+
+//  console.log(num[0]+num[1])
+
+//2
+// let num = [1,2,3]
+// let sum =[4,5,6]
+
+// console.log(num[1],sum[1])
+
+//4
+// function num(a,b){
+//     if(a==6||b==6||a+b||a-b){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+
+// }
+// console.log(num(1,5));
+//5
+// function sum (a){
+//     return "hello "+a
+// }
+// console.log(sum('iroda'));
+
+//6
+// function sum(a,b){
+//     if(a>b){
+//         return ">"
+//     }
+//     else if(a==b){
+//         return "="
+//     }
+//     else if(a<b){
+//         return "<"
+//     }
+// }
+// console.log(sum(2,3));
+
+//7
+// function num(str){
+//     return str+str+str
+// }
+// console.log(num("asa"));
+
+//8
+// let arr=[1,2,3,]
+// let sum=[4,5,6]
+// console.log(arr[0],sum[0]);
+
+//9
+// let arr=[1,2,6]
+// if(arr[0]==6||arr[arr.length-1]==6){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+//10
+// function num(start,stop){
+//   let sum =0
+//     for(let i=start;i<=stop;i++){
+//         sum+=i 
+//     }
+//     return sum
+// }
+// console.log(num(1,5))
+
+// //12
+// function num(n){
+//     let sum=0
+//     for(let i=1;i<=n;i++){
+//         sum+=i**3
+// }
+// return sum
+// }
+// console.log(num(3));
